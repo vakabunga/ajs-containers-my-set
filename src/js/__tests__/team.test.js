8b6bbeaba10d9f824bc5606e241a53d195c52451
Vakabunga <shesterov@gmail.com>
2020-05-26 01:10:07 +0300

    create tests

--- src/js/__tests__/team.test.js
+++ src/js/__tests__/team.test.js
@@ -2,7 +2,14 @@ import Team from '../team';
 import Character from '../character';
 
 describe('Team:', () => {
-  test('should create new Object of Team', () => {
+  test('should add new Character to the Team', () => {
+    const team = new Team();
+    const vaka = new Character('Vaka');
+    team.add(vaka);
+    const result = new Set();
+    expect(result.add(vaka)).toEqual(team.members);
+  });
+  test('should add several Characters to the Team', () => {
     const team = new Team();
     const vaka = new Character('Vaka');
     const bunga = new Character('Bunga');
@@ -10,6 +17,36 @@ describe('Team:', () => {
     const thrall = new Character('Thrall');
     const zombie = new Character('Zombie');
     team.addAll(vaka, bunga, zauber, thrall, zombie);
-    expect(team).toEqual();
+    const result = new Set();
+    result.add(vaka);
+    result.add(bunga);
+    result.add(zauber);
+    result.add(thrall);
+    result.add(zombie);
+    expect(result).toEqual(team.members);
+  });
+  test('should return Array of team', () => {
+    const team = new Team();
+    const vaka = new Character('Vaka');
+    const bunga = new Character('Bunga');
+    team.addAll(vaka, bunga);
+    const result = new Set();
+    result.add(vaka);
+    result.add(bunga);
+    expect([...result]).toEqual(team.toArray());
+  });
+  test('should throw error if add the same Character to team, usin team.add()', () => {
+    const team = new Team();
+    const vaka = new Character('Vaka');
+    team.add(vaka);
+    expect(() => {
+      team.add(vaka);
+    }).toThrow();
+  });
+});
+describe('Character:', () => {
+  test('should create new Character with name', () => {
+    const result = new Character('Vaka');
+    expect(result).toEqual({ name: 'Vaka' });
   });
 });
